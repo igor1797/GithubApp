b61befc5dc74d78f96baeea565bc9bad7e55f747
igor1797 <48154245+igor1797@users.noreply.github.com>
2020-12-22 16:37:40 +0100

    Changed sealed kotlin class usage.

--- app/src/main/java/igor/kuridza/dice/githubapp/model/Resource.kt
+++ app/src/main/java/igor/kuridza/dice/githubapp/model/Resource.kt
@@ -3,5 +3,5 @@ package igor.kuridza.dice.githubapp.model
 sealed class Resource{
     data class Success<T>(val data: T) : Resource()
     data class Error(val message: String?) : Resource()
-    object Loading: Resource()
+    object Loading: Resource() 
 }
